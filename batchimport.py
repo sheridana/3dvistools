531cabbed028fe81345dd72d955a1e8144e6ab8f
Arlo Sheridan <sheridana@janelia.hhmi.org>
2018-08-07 16:04:10 -0400

    Add memory constraint option to batch import

--- batchimport.py
+++ batchimport.py
@@ -25,10 +25,19 @@ class batchImport(Operator):
     which_import_method = EnumProperty( name = "Import method?", items = [('OBJ', 'OBJ', 'OBJ'), ('RAW', 'RAW', 'RAW')], default = 'OBJ', description = "Change the import method?")
     
     path_to_data = StringProperty( name="Path to data?", description="Choose a directory:", default="", maxlen=1024, subtype='DIR_PATH')
-    
-    decimate_objects = BoolProperty(name = "Decimate during import?", default = True, description = "Decimate objects as they are imported (useful for objects with high vertex counts)")
+
+    memory_constraint = BoolProperty(name = "Memory constraint?", default = False, description = "If importing very large objects (vertex count) or a lot of objects, use this method")
+
+    decimate_objects = BoolProperty(name = "Decimate?", default = False, description = "Decimate objects as they are imported (useful for objects with high vertex counts)")
     
     decimate_ratio = FloatProperty(name = "Ratio", default = 0.5, min = 0.0, max = 1.0, description = "Ratio of triangles to reduce to (uses collapse method, see useful modifiers for other decimation options)")
+
+    @classmethod
+    def poll(cls, context):
+        return context.object != []
+    
+    def check(self, context):
+        return True
     
     def execute(self, context):
         
@@ -49,22 +58,35 @@ class batchImport(Operator):
             if self.which_import_method == 'RAW':
                 from io_mesh_raw import import_raw
                 import_raw.read(path_to_file)
+                #print("Importing", item)
             else:
                 bpy.ops.import_scene.obj(filepath = path_to_file)
-                
-        for obj in bpy.data.objects:
+
+            if self.decimate_objects:
+                if self.memory_constraint:
+                    for obj in bpy.context.selected_objects:
+                        bpy.context.scene.objects.active = obj
+                        bpy.ops.object.modifier_add(type='DECIMATE')
+                        #print("(method 1) Added decimate modifier to", item)
+                        dec = obj.modifiers['Decimate']
+                        dec.ratio = self.decimate_ratio
+                        bpy.ops.object.modifier_apply(modifier='Decimate')
+                        #print("(method 1) Applying decimate modifier to", item)
+
+        if not self.memory_constraint:        
+            for obj in bpy.data.objects:
                 if obj.type == 'MESH':
                     obj.select = True
-                
-        for obj in bpy.context.selected_objects:
-            bpy.context.scene.objects.active = obj
-                
-            if self.decimate_objects:
-                bpy.ops.object.modifier_add(type='DECIMATE')
-                dec = obj.modifiers['Decimate']
-                dec.ratio = self.decimate_ratio
-                bpy.ops.object.make_links_data(type='MODIFIERS')
-                bpy.ops.object.convert(target='MESH')
+                bpy.context.scene.objects.active = obj
+
+            bpy.ops.object.modifier_add(type='DECIMATE')
+            #print("(method 2) Added decimate modifier to", obj.name)
+            dec = obj.modifiers['Decimate']
+            dec.ratio = self.decimate_ratio
+            bpy.ops.object.make_links_data(type='MODIFIERS')
+            #print("(method 2) linking modifiers")
+            bpy.ops.object.convert(target='MESH')
+            #print("(method 2) converting to mesh")
                 
         return {'FINISHED'}
                 
@@ -82,7 +104,9 @@ class batchImport(Operator):
         row.prop(self, "path_to_data")
         row = box.row(align = False)
         row.prop(self, "decimate_objects")
-        row.prop(self, "decimate_ratio")
+        if self.decimate_objects:
+            row.prop(self, "memory_constraint")
+            row.prop(self, "decimate_ratio")
         
         
         
